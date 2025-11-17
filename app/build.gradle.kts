dd0e7ed0446e489e7ec5dd0a188814b7b4ee1191
tecruz <tiagex.cruz@gmail.com>
2025-11-17 11:36:30 +0000

    Fix CI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        }
+    }
     buildFeatures {
         compose = true
     }
