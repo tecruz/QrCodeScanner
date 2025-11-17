392f7b3143ec5a5c3c779f0b1a17754bde7d0d24
tecruz <tiagex.cruz@gmail.com>
2025-11-17 13:17:15 +0000

    Add test

--- app/src/androidTest/java/com/tecruz/qrcodescanner/ui/QRCodeScannerScreenTest.kt
+++ app/src/androidTest/java/com/tecruz/qrcodescanner/ui/QRCodeScannerScreenTest.kt
@@ -1,9 +1,10 @@
 package com.tecruz.qrcodescanner.ui
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import kotlinx.coroutines.flow.MutableStateFlow
+import androidx.test.rule.GrantPermissionRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -13,6 +14,9 @@ class QRCodeScannerScreenTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
+
+    @get:Rule
+    val permissionRule: GrantPermissionRule = GrantPermissionRule.grant(android.Manifest.permission.CAMERA)
 
     @Test
     fun testQRCodeScannerScreen_noQrCode() {
@@ -36,5 +40,15 @@ class QRCodeScannerScreenTest {
         viewModel.onQrCodeScanned(qrCodeData)
 
         composeTestRule.onNodeWithText("Scanned Data: $qrCodeData").assertExists()
+    }
+
+    @Test
+    fun testCameraPreview_isDisplayed() {
+        val viewModel = QRScannerViewModel()
+        composeTestRule.setContent {
+            QRCodeScannerScreen(viewModel)
+        }
+
+        composeTestRule.onNodeWithTag("camera_preview").assertExists()
     }
 }
